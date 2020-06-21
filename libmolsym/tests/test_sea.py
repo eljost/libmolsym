4225b5f9fed670db98259c8fcb74a0dc49efa89e
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-06-21 16:02:39 +0200

    worked on tests

--- libmolsym/tests/test_sea.py
+++ libmolsym/tests/test_sea.py
@@ -1,10 +1,9 @@
 import numpy as np
+from pysisyphus.Geometry import Geometry
+import pytest
 from scipy.spatial.distance import pdist, squareform
 
-from pysisyphus.Geometry import Geometry
-from pysisyphus.helpers import geom_loader
-
-
+from libmolsym.main import find_se_atoms
 
 
 def pol2cart(r, phi):
@@ -12,11 +11,12 @@ def pol2cart(r, phi):
     return (r*np.cos(rad), r*np.sin(rad))
 
 
-def get_hexagonal_geom(r=2):
+def get_hexagonal_geom(r=2, distort=False):
     phi = np.array((30, 90, 150, 210, 270, 330))
     x, y = pol2cart(2, phi)
     z = np.zeros_like(x)
-    # y[1] = 3
+    if distort:
+        y[1] = 3
 
     atoms = "C" * x.size
     coords3d = np.stack((x, y, z), axis=1)
@@ -24,13 +24,21 @@ def get_hexagonal_geom(r=2):
     return geom
 
 
-def test_sea():
-    geom = get_hexagonal_geom()
+@pytest.mark.parametrize(
+    "distort, sea_groups", [
+        (False, 1),
+        (True, 4),
+    ]
+)
+def test_sea(distort, sea_groups):
+    geom = get_hexagonal_geom(distort=distort)
     geom.standard_orientation()
     geom.coords3d -= geom.center_of_mass
     cdm = pdist(geom.coords3d)
     dist_mat = squareform(cdm)
     se_atoms = find_se_atoms(geom.atoms, dist_mat)
+
+    assert len(se_atoms.keys()) == sea_groups
     print(se_atoms)
 
 
