bed72047324cbc666e4e22d18f646cec1091abe7
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-06-23 16:59:29 +0200

    added docstrings

--- libmolsym/dummy_data.py
+++ libmolsym/dummy_data.py
@@ -4,11 +4,15 @@ from libmolsym import SEA
 
 
 def pol2cart(r, phi):
+    """Cartesian coordinates for given polar coordinates."""
+
     rad = np.deg2rad(phi)
     return (r*np.cos(rad), r*np.sin(rad))
 
 
 def get_ring(r=2, atom_num=6, atom="C", rot=0.):
+    """Get ring with symmetrically equivalent atoms."""
+
     assert atom_num >= 3, "atom_num must be >= 3!"
     step = 360. / atom_num
     phi = (np.arange(1, atom_num+1) * step) + rot
